5cf71d63ceeb5f0cd3742e174af2caa2b29ba711
Alba Onrubia <alba.onrubia2@estudiantat.upc.edu>
2023-01-12 20:18:58 +0100

    minim2alba

---
+++ src/api/User.ts
@@ -0,0 +1,19 @@
+import userController from '../controller/userController';
+import { Router } from 'express';
+import { verifyToken, isOwner, istheUser } from '../middlewares/authJWT'
+
+const router = Router();
+
+router.post('/', userController.register); // OK
+router.post('/registergoogle', userController.registergoogle);
+router.post('/logingoogle', userController.logingoogle);
+router.get('/:id', [verifyToken], userController.profile); // OK
+router.get('/', userController.getall);  // OK
+router.put('/changepass', userController.changePass); // OK
+router.put('/updateName', [verifyToken], userController.updateName); // OK
+router.put('/updateEmail', [verifyToken], userController.updateEmail); // OK
+router.put('/activate', userController.activate); // OK
+router.delete('/', [verifyToken], userController.deleteUser); // OK
+router.put('/checkemail', userController.checkemail); // OK
+
+export default router;
